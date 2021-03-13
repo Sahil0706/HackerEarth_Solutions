f5d80ff38e7d7d891cc93b0b4a520b6447f377ea
Sahil0706 <65964632+Sahil0706@users.noreply.github.com>
2021-03-13 19:35:08 +0530

    Level : Easy
    In our school days, all of us have enjoyed the Games period. Raghav loves to play cricket and is Captain of his team. He always wanted to win all cricket matches. But only one last Games period is left in school now. After that he will pass out from school.
    
    So, this match is very important to him. He does not want to lose it. So he has done a lot of planning to make sure his teams wins. He is worried about only one opponent - Jatin, who is very good batsman.
    
    Raghav has figured out 3 types of bowling techniques, that could be most beneficial for dismissing Jatin. He has given points to each of the 3 techniques.
    
    You need to tell him which is the maximum point value, so that Raghav can select best technique.
    
    3 numbers are given in input. Output the maximum of these numbers.
    
    Input:
    
    Three space separated integers.
    
    Output:
    
    Maximum integer value

---
+++ C/Back to School.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d=0,i;
+    scanf("%d %d %d",&a,&b,&c);
+        if(d<=a)
+        {
+            d=a;
+        }
+        if(d<=b)
+        {
+            d=b;
+        }
+        if(d<=c)
+        {
+            d=c;
+        }
+        printf("%d",d);
+    return 0;
+}
