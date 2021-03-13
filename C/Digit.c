4932ce8bba3268733e31bc04cfa9cca2eabad1b0
Sahil0706 <65964632+Sahil0706@users.noreply.github.com>
2021-03-13 20:17:54 +0530

    Level : Easy

---
+++ C/Digit.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[100];
+	int i;
+	scanf("%s",s);
+	for(i=0;s[i]!=NULL;i++)
+	{
+		
+	}
+	printf("%d",i);
+	return 0;
+}#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[100];
+	int i;
+	scanf("%s",s);
+	for(i=0;s[i]!=NULL;i++)
+	{
+		
+	}
+	printf("%d",i);
+	return 0;
+}
