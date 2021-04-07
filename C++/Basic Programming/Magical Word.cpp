a970442b61467149e74bd84bfcd629df4d2ab5b4
Sahil0706 <65964632+Sahil0706@users.noreply.github.com>
2021-04-08 00:48:50 +0530

    Level : Easy
    Problem
    Dhananjay has recently learned about ASCII values.He is very fond of experimenting. With his knowledge of ASCII values and character he has developed a special word and named it Dhananjay's Magical word.
    
    A word which consist of alphabets whose ASCII values is a prime number is an Dhananjay's Magical word. An alphabet is Dhananjay's Magical alphabet if its ASCII value is prime.
    
    Dhananjay's nature is to boast about the things he know or have learnt about. So just to defame his friends he gives few string to his friends and ask them to convert it to Dhananjay's Magical word. None of his friends would like to get insulted. Help them to convert the given strings to Dhananjay's Magical Word.
    
    Rules for converting:
    
    1.Each character should be replaced by the nearest Dhananjay's Magical alphabet.
    
    2.If the character is equidistant with 2 Magical alphabets. The one with lower ASCII value will be considered as its replacement.
    
    Input format:
    
    First line of input contains an integer T number of test cases. Each test case contains an integer N (denoting the length of the string) and a string S.
    
    Output Format:
    
    For each test case, print Dhananjay's Magical Word in a new line.
    
    Constraints:
    
    1 <= T <= 100
    
    1 <= |S| <= 500

---
+++ C++/Basic Programming/Magical Word.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        int l;
+        cin>>l;
+        cin>>s;
+        for(int i=0;i<=l;i++)
+        {
+            //scanf("%c",&s[i]);
+            if(s[i]=='A'|s[i]=='B'|s[i]=='D'|s[i]=='E')
+            {
+                s[i]='C';
+            }
+            if(s[i]=='F'|s[i]=='H')
+            {
+                s[i]='G';
+            }
+            if(s[i]=='J'|s[i]=='K'|s[i]=='L')
+            {
+                s[i]='I';
+            }
+            if(s[i]=='M'|s[i]=='N'|s[i]=='P'|s[i]=='Q')
+            {
+                s[i]='O';
+            }
+            if(s[i]=='R'|s[i]=='T'|s[i]=='U'|s[i]=='V')
+            {
+                s[i]='S';
+            }
+            if(s[i]=='W'|s[i]=='X'|s[i]=='Z')
+            {
+                s[i]='Y';
+            }
+
+            if(s[i]=='b'|s[i]=='c')
+            {
+                s[i]='a';
+            }
+            if(s[i]=='d'|s[i]=='f')
+            {
+                s[i]='e';
+            }
+            if(s[i]=='h'|s[i]=='i')
+            {
+                s[i]='g';
+            }
+            if(s[i]=='j'|s[i]=='l')
+            {
+                s[i]='k';
+            }
+            if(s[i]=='n'|s[i]=='o')
+            {
+                s[i]='m';
+            }
+            if(s[i]=='p'|s[i]=='r'|s[i]=='s'|s[i]=='t'|s[i]=='u'| s[i]=='v'|s[i]=='w'|s[i]=='x'|s[i]=='y'|s[i]=='z')
+            {
+                s[i]='q';
+            }
+            if(s[i] == '0'| s[i] == '1' | s[i] == '2' | s[i] == '3' | s[i] == '4'| s[i] == '5' | s[i] == '6' | s[i] == '7' | s[i] == '8' | s[i] == '9' ){
+                s[i] = 'C';
+            }
+            if(int(s[i]) < 66){
+                s[i] = 'C';
+            }
+            if(int(s[i]) > 89 && int(s[i]) < 94 ){
+                s[i] = 'Y';
+            }
+            if(int(s[i]) > 93 && int(s[i]) < 98 ){
+                s[i] = 'a';
+            }
+            if(int(s[i]) > 113 ){
+                s[i] = 'q';
+            }
+            //printf("%c",s[i]);
+        }
+        cout<<s<<"\n";
+    }
+    return 0;
+}
